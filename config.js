14fa0f8e056758be847e4cdcace621e57d486943
yuhuiyuan <yuhuiyuan@youplus.cc>
2018-03-22 17:56:14 +0800

    修改部署配置

---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+    FACTORY_ADDRESS: '0x3090f626Fb89e372517ce1acE8217b476cC0B3A4',
+    MNEMONIC:        '',
+    PROVIDER_URI:    'https://rinkeby.infura.io/gIHHQoyDKlrEVwxPxaHU'
+};
+
+module.exports = config;
